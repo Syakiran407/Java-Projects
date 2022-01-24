cf71c6c12a27834e3ffed2e974567271e5c6f98e
Mahmud Syakiran <mahmudsyakiran407@gmail.com>
2022-01-25 00:14:00 +0800

    praticing array list

---
+++ JavaProfessionalCourse/src/com/kiranmetz/ArrayTest.java
@@ -0,0 +1,22 @@
+package com.kiranmetz;
+
+import java.util.Scanner;
+
+public class ArrayTest {
+
+    public static void main (String [] args) {
+
+       int[] arr = {0, 1, 2};
+       int a, b, c, d;
+
+       //var[] m = {0, 1, 2};
+
+       a = b = c = d = 1;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
